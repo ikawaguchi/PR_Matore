44c5c46e4130d2c6b32bf99e723b47c1db824bea
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2014-09-06 17:13:56 +0900

    HTMLパーサーで解析テスト

--- PartyRocketsMatome/ViewController.m
+++ PartyRocketsMatome/ViewController.m
@@ -18,15 +18,6 @@
 {
     [super viewDidLoad];
     
-    NSURL *url = [NSURL URLWithString:@"http://rssblog.ameba.jp/partyrockets/rss20.xml"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"%@", operation.responseString);
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"%@", error.localizedDescription);
-    }];
-    [operation start];
 
 }
 
---
+++ PartyRocketsMatome/source/ViewController/PRTBlogTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PRTBlogTableViewController.h
+//  PartyRocketsMatome
+//
+//  Created by 川口功 on 2014/09/06.
+//  Copyright (c) 2014年 I.K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRTBlogTableViewController : UITableViewController
+
+@end
